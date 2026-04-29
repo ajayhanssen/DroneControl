7608b0fe1ac173025869abc92e1892a40e4b54ac
ajayhanssen <andi.thoeni@gmail.com>
2026-04-29 17:46:15 +0200

    ES GEAT

--- plot_transform.m
+++ plot_transform.m
@@ -1,10 +1,10 @@
 t = out.simout.Time;
-x = out.simout.Data(1,:,:);
-y = out.simout.Data(3,:,:);
-z = out.simout.Data(5,:,:);
-phi = out.simout.Data(7,:,:);
-theta = out.simout.Data(9,:,:);
-psi = out.simout.Data(11,:,:);
+x = out.simout.Data(:,1);
+y = out.simout.Data(:,2);
+z = out.simout.Data(:,3);
+phi = out.simout.Data(:,7);
+theta = out.simout.Data(:,8);
+psi = out.simout.Data(:,9);
 
 axes( 'DataAspectRatio', [1 1 1], 'View', [37.5 30] )
 for i = 1:length(x)
@@ -12,7 +12,9 @@ for i = 1:length(x)
     rot = eul2rotm([phi(i), theta(i), psi(i)], 'XYZ');
     trans = [x(i), y(i), z(i)];
     hold on
-    plotframe( rot, trans )
+    if mod(i, 10)==0
+        plotframe( rot, trans )
+    end
 end
 
 grid on
